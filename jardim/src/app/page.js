6ba7c7f2d3d14388d1ce95cdc93cd530c9e2c1a1
GabrielJM2023 <jardimgabriel2022@gmail.com>
2024-09-30 15:42:22 -0300

    Habilidades

--- jardim/src/app/page.js
+++ jardim/src/app/page.js
@@ -2,6 +2,15 @@ import { FaJedi } from "react-icons/fa6";
 import { FaGithub } from "react-icons/fa6";
 import { FaLinkedinIn } from "react-icons/fa6";
 import { MdWhatsapp } from "react-icons/md";
+import { SiDelphi } from "react-icons/si";
+import { FaReact } from "react-icons/fa";
+import { IoLogoJavascript } from "react-icons/io5";
+import { SiTypescript } from "react-icons/si";
+import { FaNodeJs } from "react-icons/fa";
+import { FaHtml5 } from "react-icons/fa";
+import { FaCss3Alt } from "react-icons/fa";
+import { FaDatabase } from "react-icons/fa";
+import { DiScrum } from "react-icons/di";
 
 export default function Home() {
   return (
@@ -10,22 +19,23 @@ export default function Home() {
         <div className="menuBar">
           <div className="IconNome">
             <a href="#"><FaJedi/></a>
-            <a href="#">Jardim</a>                      
+            <a href="#">G. Jardim</a>
             </div>
           <nav>          
             <ul className="menu">
-              <li><a href="#">Início</a></li>
-              <li><a href="#">Sobre</a></li>
+              <li><a href="#home">Início</a></li>
+              <li><a href="#sobre">Sobre</a></li>
+              <li><a href="#habilidades">Habilidades</a></li>
               <li><a href="#">Trabalhos</a></li>
               <li><a href="#">Contato</a></li>
             </ul>
           </nav>  
-        </div>        
+        </div>
       </header>
       
-      <section className="inicio">
+      <section className="inicio" >
         <div className="TituloInicio">
-          <h1>Gabriel Jardim</h1>
+          <h1 id="home">Gabriel Jardim</h1>
           <h2>Desenvolvedor Front-End</h2>  
           <div className="IconsTitulo">
             <FaGithub className="IconTitulo"/>
@@ -35,14 +45,14 @@ export default function Home() {
         </div>        
       </section> 
 
-      <div className="TituloCentro">
+      <div className="TituloCentro" id="sobre">
           <div className="Titulo">
             <h1>Sobre</h1>
           </div>
       </div>
 
-      <section className="Sobre">        
-        <div className="ImagemSobre">          
+      <section className="Sobre" >
+        <div className="ImagemSobre">
           <img src='https://github.com/GabrielJM2023.png' className='Avatar'/>
         </div>
         <div className="AreaSobre">
@@ -53,16 +63,61 @@ export default function Home() {
         </div>
       </section>   
 
-      <div className="TituloCentro">
+      <div className="TituloCentro" id="habilidades">
           <div className="Titulo">
-            <h1>Trabalhos</h1>
+            <h1>Habilidades</h1>
           </div>          
       </div>
 
+      <section className="Habilidades">
+        <div className="LadoEsquerdo">   
+          <div className="ContainerIcon">          
+            <div className="AreaIcon Delphi">
+              <SiDelphi className="Icon"/>
+            </div>  
+            
+            <div className="AreaIcon React">
+              <FaReact className="Icon"/>
+            </div>            
+            <div className="AreaIcon">
+              <IoLogoJavascript className="Icon"/>
+            </div>                        
+            <div className="AreaIcon">
+              <SiTypescript className="Icon"/>              
+            </div>          
+            <div className="AreaIcon">
+              <FaNodeJs className="Icon"/>              
+            </div>          
+            <div className="AreaIcon">
+              <FaHtml5 className="Icon"/>              
+            </div>          
+            <div className="AreaIcon">
+              <FaCss3Alt className="Icon"/>              
+            </div>          
+            <div className="AreaIcon">
+              <FaDatabase className="Icon"/>              
+            </div>          
+            <div className="AreaIcon">
+              <FaGithub className="Icon"/>              
+            </div>          
+            <div className="AreaIcon">
+              <DiScrum className="Icon"/>  
+            </div>                    
+          </div>        
+        </div>
+        <div className="LadoDireito">
+          <div className="AreaTexto">
+            <h1 className="DefaultText">Teste</h1>
+            <h1 className="Text TextDelphi">Dlephi</h1> 
+            <h1 className="Text TextReact">Reac</h1> 
+          </div>
+        </div>      
+      </section>
+
 
       <div className="TituloCentro">
           <div className="Titulo">
-            <h1>Contato</h1>
+            <h1>Trabalhos</h1>
           </div>
       </div>
 
